c0c28939b8d809dc736d182cf04a393e7c40dffd
gustavo <gustavozegarra998@hotmail.com>
2020-05-03 17:14:02 -0500

    validación en conductores

--- Revifast/Models/Conductor.cs
+++ Revifast/Models/Conductor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Revifast.Models
 {
@@ -12,10 +13,19 @@ namespace Revifast.Models
 
         public int ConductorId { get; set; }
         public string Usuario { get; set; }
+        [Required(ErrorMessage = "Nombres es requerido")]
+        [DataType(DataType.Text)]
         public string Nombres { get; set; }
+        [Required(ErrorMessage = "Apellidos es requerido")]
+        [DataType(DataType.Text)]
         public string Apellidos { get; set; }
+        [Required(ErrorMessage = "DNI es requerido")]
+        [StringLength(8,MinimumLength = 8,ErrorMessage ="DNI requiere 8 caracteres")]
         public string Dni { get; set; }
         public string Correo { get; set; }
+        [Required(ErrorMessage = "Celular es requerido")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Celular requiere 9 caracteres")]
+        [DataType(DataType.PhoneNumber)]
         public string Celular { get; set; }
 
         public virtual ICollection<Vehiculo> Vehiculo { get; set; }
