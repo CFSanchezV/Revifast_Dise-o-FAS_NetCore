c7b5bb144e844263ccb1ce028ff81b1e17f2ee0e
xeroot <r10500@hotmail.com>
2020-05-06 17:16:55 -0500

    Conductor fixeado

--- Revifast/Models/Conductor.cs
+++ Revifast/Models/Conductor.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 
 namespace Revifast.Models
 {
@@ -12,22 +14,37 @@ namespace Revifast.Models
         }
 
         public int ConductorId { get; set; }
+
         public string Usuario { get; set; }
+
         [Required(ErrorMessage = "Nombres es requerido")]
         [DataType(DataType.Text)]
         public string Nombres { get; set; }
+
         [Required(ErrorMessage = "Apellidos es requerido")]
         [DataType(DataType.Text)]
         public string Apellidos { get; set; }
+
         [Required(ErrorMessage = "DNI es requerido")]
-        [StringLength(8,MinimumLength = 8,ErrorMessage ="DNI requiere 8 caracteres")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "DNI requiere 8 caracteres")]
         public string Dni { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Correo { get; set; }
-        [Required(ErrorMessage = "Celular es requerido")]
-        [StringLength(9, MinimumLength = 9, ErrorMessage = "Celular requiere 9 caracteres")]
-        [DataType(DataType.PhoneNumber)]
+
+        [Phone, Required(ErrorMessage = "Celular es requerido")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "El celular requiere 9 dígitos")]
         public string Celular { get; set; }
 
         public virtual ICollection<Vehiculo> Vehiculo { get; set; }
+
+        public bool IsDniValid() => Dni.Length == 8;
+
+        public bool IsPhoneValid()
+        {
+            var pattern = @"(?:^|\D)(\d{9})(?!\d)";
+            var match = Regex.Match(Celular, pattern);
+            return match.Success;
+        }
     }
 }
